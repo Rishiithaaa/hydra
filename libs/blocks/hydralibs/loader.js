72fcb89a1faed97ea257461324c1dcd32fbb6978
Amit Bikram Sarangi <amitbtalk@gmail.com>
2025-04-26 22:43:14 +0530

    sample

--- libs/blocks/hydralibs/loader.js
+++ libs/blocks/hydralibs/loader.js
@@ -1,1 +1,6 @@
-alert('222')
+import './accordion-hydrate.js';
+import './global-navigation-hydrate.js';
+
+import './modal-hydrate.js';
+import './section-metadata-hydrate.js';
+import './tabs-hydrate.js';
